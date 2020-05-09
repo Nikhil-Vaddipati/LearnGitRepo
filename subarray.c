e7883d90da535748ab38beb11b30f7edbd764c2e
nikhil-vv <nvaddipati@gmail.com>
2020-05-09 13:38:14 +0530

    sub array

---
+++ subarray.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+int main()
+{
+    int a[5]={1,2,3,4,5};
+    int i,j,sum,k;
+
+    for(i=0;i<5;i++)
+    {
+            for(j=i;j<5;j++)
+            {
+                for(k=i;k<=j;k++)
+                {
+                    printf("%d ",a[k]);
+                }
+                printf("\n");
+            }
+    }
+
+
+
+}
